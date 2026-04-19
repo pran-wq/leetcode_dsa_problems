ff54bc4da4406e57fe15518fc9060c0abe186351
PRANAV RAJ <114560034+pran-wq@users.noreply.github.com>
2026-04-19 23:56:41 +0530

    Refactor can_construct function and enhance documentation
    Updated comments and improved frequency check in can_construct function.

--- 383_ransom_note.py
+++ 383_ransom_note.py
@@ -1,16 +1,24 @@
-'''Given two strings ransomNote and magazine,
-return True if ransomNote can be constructed 
-using letters from magazine.
-Each letter in magazine can only be used ONCE!'''
+# LeetCode 383 - Ransom Note
+# https://leetcode.com/problems/ransom-note/
+#
+# Description:
+# Given two strings ransomNote and magazine,
+# return True if ransomNote can be constructed
+# using letters from magazine.
+# Each letter in magazine can only be used once.
+#
+# Technique: Hash Map / Frequency Count
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 
 def can_construct(r,m):
     freq={}
     for char in m:
         freq[char]=freq.get(char,0)+1
     for char in r:
-        if char  not in m:
+        if char  not in freq:
             return False
-        elif freq[char]<0:
+        elif freq[char]<1:
             return False
         freq[char]-=1
     else:
