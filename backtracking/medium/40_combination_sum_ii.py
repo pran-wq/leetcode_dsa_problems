02368042c1e4efb01130ce554dd3efe332dcd95f
PRANAV RAJ <114560034+pran-wq@users.noreply.github.com>
2026-08-19 12:43:29 +0530

    Add combinationSum function for unique combinations
    Implement backtracking to find unique combinations that sum to target.

--- backtracking/medium/40_combination_sum_ii.py
+++ backtracking/medium/40_combination_sum_ii.py
@@ -1,1 +1,18 @@
+def combinationSum(candidates,target):
+    candidates.sort()
+    results=[]
+    def backtrack(start, current_target,path):
+        if current_target==0:
+            results.append(list(path))
+            return
 
+        for i in range (len(candidates)-1):
+            if candidates[i] > current_target:
+                break 
+        
+            if i >start and candidates[i] == candidates[i+1]:
+                continue
+            backtrack(candidates[i+1], current_target-candidates[i], path)
+            path.pop()
+        backtrack(0,target,[])
+        return results
