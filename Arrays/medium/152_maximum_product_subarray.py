17f6f8ed008d88fcb11b48eb0b8c6b8d22c34ed2
PRANAV RAJ <114560034+pran-wq@users.noreply.github.com>
2026-08-20 14:06:29 +0530

    Implement maximum product subarray solution

---
+++ Arrays/medium/152_maximum_product_subarray.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        max_product = nums[0]
+        current_product = 1
+       
+        for x in nums:
+            current_product = current_product * x
+            max_product = max(current_product, max_product)
+            if current_product ==0:
+                current_product = 1
+        current_product =1
+        for num in reversed(nums):
+            current_product *=num
+            max_product = max(max_product, current_product)
+            if current_product ==0:
+                current_product =1
+            
+        return max_product
